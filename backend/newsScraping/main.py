e9ce1c0f4b6b31e5694363c374ffd75ee478fed7
DanteT03 <93191432+DanteT03@users.noreply.github.com>
2025-11-29 16:13:29 +0100

    ADD: check loop for new jobs

--- backend/newsScraping/main.py
+++ backend/newsScraping/main.py
@@ -75,30 +75,38 @@ if __name__ == "__main__":
     max_attempts = 3
     backoff = 0.5
     nr_of_jobs = 2
-    jobs = None
-    for attempt in range(1, max_attempts + 1):
-        try:
-            jobs = manager.get_work_batch(nr_of_jobs)
-        except Exception as exc:
-            print(f"Attempt {attempt} failed when fetching jobs: {exc}")
-            if attempt < max_attempts:
-                time.sleep(backoff * attempt)
+    interval_between_job_checks = 1800 # Recheck for jobs every 30 minutes
+    try:
+        while True:
+            jobs = None
+            for attempt in range(1, max_attempts + 1):
+                try:
+                    jobs = manager.get_work_batch(nr_of_jobs)
+                except Exception as exc:
+                    print(f"Attempt {attempt} failed when fetching jobs: {exc}")
+                    if attempt < max_attempts:
+                        time.sleep(backoff * attempt)
+                    else:
+                        print("Max attempts reached; aborting.")
+
+            # If no jobs returned or an error occurred and jobs is still None, do nothing
+            if not jobs:
+                print("No jobs to process.")
+                time.sleep(interval_between_job_checks)
+                continue
             else:
-                print("Max attempts reached; aborting.")
+                # Map jobs to spider classes using spider_map
+                spider_classes = []
+                for job in jobs:
+                    spider = get_spider_by_name(job)
+                    spider_classes.append(spider)
 
-    # If no jobs returned or an error occurred and jobs is still None, do nothing
-    if not jobs:
-        print("No jobs to process.")
-    else:
-        # Map jobs to spider classes using spider_map
-        spider_classes = []
-        for job in jobs:
-            spider = get_spider_by_name(job)
-            spider_classes.append(spider)
+                print(spider_classes)
 
-        print(spider_classes)
-
-        run_parallel_spiders_for_list(spider_classes)
+                run_parallel_spiders_for_list(spider_classes)
+    
+    except KeyboardInterrupt:
+        print("Shut down by user.")
     # seq_res = run_benchmark_spiders(run_spiders)
     # par_res = run_benchmark_spiders(run_parallel_spiders)
     # print(f"Sequential times: {seq_res}")
